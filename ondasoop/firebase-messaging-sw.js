5d4a39d0062e4e388bedc10538103c4862a611b1
Jeong Yo Han <ingyeoking13@gmail.com>
2020-11-29 16:25:34 +0900

    asdf

--- ondasoop/firebase-messaging-sw.js
+++ ondasoop/firebase-messaging-sw.js
@@ -40,18 +40,17 @@ const messaging = firebase.messaging();
 // background (Web app is closed or not in browser focus) then you should
 // implement this optional method.
 // [START on_background_message]
-/*
 messaging.onBackgroundMessage(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-   Customize notification here
+  /*
+   //Customize notification here
    const notificationTitle = 'Background Message Title';
    const notificationOptions = {
      body: 'Background Message body.',
      icon: '/firebase-logo.png'
    };
+   */
 
-   self.registration.showNotification(notificationTitle,
-     notificationOptions);
+//   self.registration.showNotification();
 });
-*/
 // [END on_background_message]
